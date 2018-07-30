46507f860a336e981fde0a3053fd6b7961cdc6b0
Eric Erhardt <eric.erhardt@microsoft.com>
2018-07-30 18:03:42 -0500

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,124 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading.Tasks;
+
+namespace MLFunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            if (typeof(Microsoft.ML.Runtime.Data.LoadTransform) != 
+                typeof(Microsoft.ML.Runtime.Learners.LinearClassificationTrainer))
+            {
+                log.Info("Assemblies are loaded perfectly");
+            }
+
+            //GitHubIssue issue = data.Issue;
+            //List<object> labels = issue.Labels;
+
+            //if (data.Action == "opened" && labels.Count == 0)
+            //{
+            string title = "msbuild /T:BuildAndTest /P:TargetGroup=netfx throws PNSE with Ref-Emit";
+                int number = 31485;
+                #region body
+                string body = @"Running `msbuild /T:BuildAndTest /P:TargetGroup=netfx` causes a PNSE to be thrown in the following example.
+
+This is a real example from the VB tests that had to be nerfed because of this
+```cs
+public static IEnumerable<object[]> InvalidBool_TestData()
+{
+    if (PlatformDetection.IsReflectionEmitSupported)
+    {
+        object floatEnum = null;
+        try
+        {
+            floatEnum = FloatEnum;
+        }
+        catch (PlatformNotSupportedException)
+        {
+            yield break;
+        }
+
+        yield return new object[] { floatEnum };
+        yield return new object[] { DoubleEnum };
+        yield return new object[] { BoolEnum };
+        yield return new object[] { CharEnum };
+        yield return new object[] { IntPtrEnum };
+        yield return new object[] { UIntPtrEnum };
+    }
+}
+private static object s_floatEnum;
+
+public static object FloatEnum
+{
+    get
+    {
+        if (s_floatEnum == null)
+        {
+            AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(""Name""), AssemblyBuilderAccess.RunAndCollect);
+            ModuleBuilder module = assembly.DefineDynamicModule(""Name"");
+
+                EnumBuilder eb = module.DefineEnum(""CharEnumType"", TypeAttributes.Public, typeof(float));
+                eb.DefineLiteral(""A"", 1.0f);
+                eb.DefineLiteral(""B"", 2.0f);
+                eb.DefineLiteral(""C"", 3.0f);
+
+                s_floatEnum = Activator.CreateInstance(eb.CreateTypeInfo());
+            }
+
+            return s_floatEnum;
+        }
+    }
+
+    private static object s_doubleEnum;
+
+    public static object DoubleEnum
+    {
+        get
+        {
+            if (s_doubleEnum == null)
+            {
+                AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(""Name""), AssemblyBuilderAccess.RunAndCollect);
+                ModuleBuilder module = assembly.DefineDynamicModule(""Name"");
+
+                EnumBuilder eb = module.DefineEnum(""CharEnumType"", TypeAttributes.Public, typeof(double));
+                eb.DefineLiteral(""A"", 1.0);
+                eb.DefineLiteral(""B"", 2.0);
+                eb.DefineLiteral(""C"", 3.0);
+
+                s_doubleEnum = Activator.CreateInstance(eb.CreateTypeInfo());
+            }
+
+            return s_doubleEnum;
+        }
+    }
+
+    private static object s_boolEnum;
+```";
+                #endregion
+                log.Info($"A {number.ToString()} issue with {title} has been opened.");
+
+                var corefxIssue = new GitHubIssue
+                {
+                    ID = number.ToString(),
+                    Title = title,
+                    Description = body
+                };
+
+                string label = await Predictor.PredictAsync(corefxIssue, log);
+                log.Info($"Labeling completed: {label}");
+            //}
+            //else
+            //{
+            //    log.Info($"The issue {issue.Number.ToString()} is already opened or it already has a label");
+            //}
+
+            return new OkObjectResult(label);
+        }
+    }
+}
---
+++ Predictor.cs
@@ -0,0 +1,69 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ML;
+using Microsoft.ML.Runtime.Api;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace MLFunction
+{
+    public class GitHubIssue
+    {
+        [Column(ordinal: "0")]
+        public string ID;
+
+        [JsonIgnore]
+        [Column(ordinal: "1")]
+        public string Area;
+
+        [Column(ordinal: "2")]
+        public string Title;
+
+        [DataMember(Name = "body")]
+        [Column(ordinal: "3")]
+        public string Description;
+
+        [DataMember(Name = "labels")]
+        public List<object> Labels { get; set; }
+
+        [DataMember(Name = "number")]
+        public int Number { get; set; }
+    }
+
+    public class GitHubIssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Area;
+
+        [ColumnName("Score")]
+        public float[] Probabilities;
+    }
+
+    internal static class Predictor
+    {
+        private static string ModelPath => Path.Combine(
+            Path.GetDirectoryName(typeof(Predictor).Assembly.Location),
+            "..",
+            "model",
+            "GitHubIssueLabelerModel.zip");
+
+        public static async Task<string> PredictAsync(GitHubIssue issue, TraceWriter log)
+        {
+            PredictionModel<GitHubIssue, GitHubIssuePrediction> model = await PredictionModel.ReadAsync<GitHubIssue, GitHubIssuePrediction>(ModelPath);
+            GitHubIssuePrediction prediction = model.Predict(issue);
+
+            float[] probabilities = prediction.Probabilities;
+            float maxProbability = probabilities.Max();
+            log.Info($"Label {prediction.Area} for {issue.ID} is predicted with confidence {maxProbability.ToString()}");
+
+            return maxProbability > 0.8 ? prediction.Area : null;
+        }
+    }
+}
